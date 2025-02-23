1768abb12453b43ff7c47bd067c7b782252a8b06
luigofull <githubars441@gmail.com>
2025-02-23 20:26:15 +0200

    added greetinig message

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+        rf"Привет! 👋 Я твой персональный бот-напоминалка. Помогу не забыть важные дела, встречи и задачи. Просто напиши, что и когда мне напомнить, а я обо всём позабочусь! ⏰😉",
     )
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -20,15 +19,11 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
-async def kek(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("kek")
-
 def main() -> None:
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("pomogi", help_command))
-    application.add_handler(CommandHandler("kek", kek))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
