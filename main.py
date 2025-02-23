54e1320cdf49508eaf5e59b2f0aa556102c09a7b
luigofull <githubars441@gmail.com>
2025-02-23 20:38:20 +0200

    Greetings update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import Hellper
 from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
@@ -10,22 +11,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет! 👋 Я твой персональный бот-напоминалка. Помогу не забыть важные дела, встречи и задачи. Просто напиши, что и когда мне напомнить, а я обо всём позабочусь! ⏰😉",
+        Hellper.GREETINGS
     )
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Pomogayu")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
 
 def main() -> None:
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("pomogi", help_command))
-
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
