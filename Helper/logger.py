71e26d034911822a82edf5640d2cf6f1cb00dfba
luigofull <githubars441@gmail.com>
2025-07-05 21:53:26 +0300

    asdad

--- Helper/logger.py
+++ Helper/logger.py
@@ -2,7 +2,7 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-Format = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+Format = '%(asctime)s %(message)s'
 logging.basicConfig(format=Format,filename='.\logs\status.log', encoding='utf-8', level=logging.INFO)
 
 class Log:
