b5baf5222afa1435524dfc876619081522754979
luigofull <githubars441@gmail.com>
2025-07-05 20:38:11 +0300

    aaaa

--- Helper/logger.py
+++ Helper/logger.py
@@ -2,9 +2,9 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+Format = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+logging.basicConfig(format=Format,filename='.\logs\status.log', encoding='utf-8', level=logging.INFO)
 
 class Log:
 	def log(self, а):
-		logger.debug(а)
+		logger.info(а)
